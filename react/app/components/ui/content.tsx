3a09adaabfe7c72bacce651a58de8b42ca21666c
jokoson <k@k.kk>
2026-01-02 14:07:03 +0100

    fix

--- react/app/components/ui/content.tsx
+++ react/app/components/ui/content.tsx
@@ -1,10 +1,9 @@
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
-import styles from "./content.module.css";
 
 export const Content = ({data}: { data: any }) => {
     if (!data?.wysiwyg) return null;
     return (
-        <div className={styles.Content}>
+        <div>
             <BlocksRenderer content={data.wysiwyg} />
         </div>
     )
